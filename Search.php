cc4d25d82999dc4daf207a517bef68b6e85e71b2
Jabin <jpyan727@2906.com>
2019-07-04 21:00:11 +0800

    二分查找递归方式

--- Search.php
+++ Search.php
@@ -18,7 +18,8 @@ class Search
 		return -1;
 	}
 
-	public function binarySearch(array $array, int $needle)
+	//二分查找遍历方式
+	public function binarySearch1(array $array, int $needle)
 	{
 		sort($array);
 		$lIndex = 0;
@@ -34,6 +35,24 @@ class Search
 			}
 		}
 		return -1;
+	}
+
+	//二分查找递归方式
+	public function binarySearch2(array $array, int $needle,int $lIndex, int $rIndex)
+	{
+		sort($array);
+		$size = count($array);
+		$mid = intval(($lIndex+$rIndex)/2);
+		if ($lIndex>=$rIndex) {
+			return -1;
+		}
+		if ($needle == $array[$mid]) {
+			return $mid;
+		}elseif($needle >= $array[$mid]){
+			return $this->binarySearch2($array,$needle,$mid+1,$size-1);
+		}else{
+			return $this->binarySearch2($array,$needle,0,$mid-1);
+		}
 	}
 
 	public function binarySearchMulti(array $array, int $needle)
@@ -62,14 +81,19 @@ $testArr = [1,5,8,8,8,2,3,9];
 $needle = 8;
 
 $linearIndex = $searchObj->linearSearch($testArr,$needle);
-$binaryIndex = $searchObj->binarySearch($testArr,$needle);
+$binaryIndex1 = $searchObj->binarySearch1($testArr,$needle);
+$binaryIndex2 = $searchObj->binarySearch2($testArr,$needle,0,count($testArr)-1);
+
 $binaryMultiIndex = $searchObj->binarySearchMulti($testArr,$needle);
 
 echo "=======================\nLineay Search\n=======================\n";
 echo "$linearIndex";
 
-echo "\n=======================\nBinary Search\n=======================\n";
-echo "$binaryIndex";
+echo "\n=======================\nBinary1 Search\n=======================\n";
+echo "$binaryIndex1";
+
+echo "\n=======================\nBinary2 Search\n=======================\n";
+echo "$binaryIndex2";
 
 echo "\n=======================\nBinary Multi Search\n=======================\n";
 echo "$binaryMultiIndex";
